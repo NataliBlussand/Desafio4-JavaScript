db5e1c26985747f24e8981833eea5f61d43d576b
Natali Blussand <nataliblussand@gmail.com>
2021-11-10 01:25:26 -0300

    desafio 4

---
+++ js/desafio4.js
@@ -0,0 +1,13 @@
+function solicitarNombre(){
+    let nombreUsuario = prompt("Ingresa tu nombre!");
+    alert("Hola " + nombreUsuario + "! \nBienvenido/a a FAVISA!");
+} 
+
+solicitarNombre();
+
+function solicitarMail(){
+    let mailUsuario = prompt ("Deja tu mail para recibir novedades de nuestros productos!");
+    alert("Gracias por tu interés! \nEsperamos que tengas una buena experiencia en nuestra web!");
+}
+
+solicitarMail();
